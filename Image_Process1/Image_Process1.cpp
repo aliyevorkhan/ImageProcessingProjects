381af8d56bd3eaaa18cb25a89e02c3694eb658f9
Orkhan ALIYEV <338396@ogr.ktu.edu.tr>
2019-02-08 09:45:42 -0800

    son degisiklikler yapildi

--- Image_Process1/Image_Process1.cpp
+++ Image_Process1/Image_Process1.cpp
@@ -17,7 +17,7 @@ int _tmain(int argc, _TCHAR *argv[])
 	int Width, Height;
 	long Size, new_size;
 
-	input = L"C://Users//Orkhan ALIYEV//Desktop//deneme//indir.bmp"; // bu path dosya yuklemek icin
+	input = L"C://Users//Orkhan ALIYEV//Read_Image//fotograflar//renkli.bmp"; // bu path dosya yuklemek icin
 
 	BYTE *buffer = LoadBMP(&Width, &Height, &Size, input);
 	BYTE *raw_intensity = DrawLine(buffer, Width, Height);
@@ -29,7 +29,7 @@ int _tmain(int argc, _TCHAR *argv[])
 	{
 
 		BYTE *display_imge = ConvertIntensityToBMP(raw_intensity, Width, Height, &new_size);
-		output = L"C://Users//Orkhan ALIYEV//Desktop//deneme//copy.bmp"; // but path dosyayi kaydetmek icin
+		output = L"C://Users//Orkhan ALIYEV//Read_Image//fotograflar//copy.bmp"; // but path dosyayi kaydetmek icin
 		if (SaveBMP(display_imge, Width, Height, new_size, output))
 			cout << " Output Image was successfully saved" << endl;
 		else
